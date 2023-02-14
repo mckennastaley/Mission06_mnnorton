143b581bd4e7122d7680c664cf996590542b3993
McKenna Staley <mnnorton@byu.edu>
2023-02-13 21:57:19 -0700

    Add project files.

---
+++ Mission06_mnnorton/Models/ApplicationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_mnnorton.Models
+{
+    public class ApplicationResponse
+    {
+        [Key]
+        [Required]
+        public int MovieID { get; set; }
+        public string MovieTitle { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Mission06_mnnorton/Models/MovieContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_mnnorton.Models
+{
+    public class MovieContext : DbContext 
+    {
+        //constructor
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            //leave blank for  now
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+    }
+}
